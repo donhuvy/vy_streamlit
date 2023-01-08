7fa3c85310ca1639a30127a7835c32d5c62a2884
Do Nhu Vy <donhuvy@outlook.com>
2023-01-08 18:36:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 print("em yeu")
+
+st.title('The Titanic dataset')
+df = pd.read_csv("second.csv")
+
+st.subheader("The data")
+st.write(df)
+
+st.subheader("Some visualizations")
